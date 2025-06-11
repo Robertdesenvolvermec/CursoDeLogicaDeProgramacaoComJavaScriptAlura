cab91e70614c7bf66ffa38d0fa8f1114f096ca01
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-06-11 20:56:39 -0300

    Comecei um novo curso de lógica de programação

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Bem vindo ao jogo do número secreto");
+
+let numeroSecreto = prompt("Digite seu número entre 1 e 30");
+
+if(numeroSecreto == 29){
+    alert("Você acertou!!!");
+} else {
+    alert("Você Errou, tente novamente.");
+};
