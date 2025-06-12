547850cdb15afcf7d28aadc15b58d79ccdd17dc8
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-06-12 18:24:05 -0300

    Atualizei a condição da validação do número

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ let numeroSecreto = 5;
 let chute = prompt("Digite seu número entre 1 e 30");
 
 if( chute == numeroSecreto){
-    console.log("Você acertou!! O número secreto é o 5.");
+    alert("Você acertou!! O número secreto é o 5.");
+    console.log('Você acertou!! O número secreto é o ' + numeroSecreto);
+} else {
+    alert("Você errou, tente novamente.");
+    console.log('Usuário chutou número ' + chute);
 };
 
 //Nova linha
