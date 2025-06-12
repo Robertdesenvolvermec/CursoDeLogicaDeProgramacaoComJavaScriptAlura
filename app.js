94463ac9e4455b7fca8259e30809991766861752
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-06-12 02:46:51 -0300

    Adicionei a linha console.log

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 alert("Bem vindo ao jogo do número secreto");
 
-let numeroSecreto = prompt("Digite seu número entre 1 e 30");
+let numeroSecreto = 5;
+let chute = prompt("Digite seu número entre 1 e 30");
 
-if(numeroSecreto == 29){
-    alert("Você acertou!!!");
-} else {
-    alert("Você Errou, tente novamente.");
+if( chute == numeroSecreto){
+    console.log("Você acertou!! O número secreto é o 5.");
 };
