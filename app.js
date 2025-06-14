ed178f7779999075cc1dafc8b2875deb1e18740f
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-06-13 22:33:07 -0300

    Adicionei a linha de tentativas

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 alert("Bem vindo ao jogo do número secreto");
 
-let numeroSecreto = 5;
-let chute = prompt("Digite seu número entre 1 e 30");
+let numeroSecreto = 4;
+let chute;
+let tentativas = 1;
 
-if( chute == numeroSecreto){
-    alert("Você acertou!! O número secreto é o 5.");
-    console.log('Você acertou!! O número secreto é o ' + numeroSecreto);
-} else {
-    alert("Você errou, tente novamente.");
-    console.log('Usuário chutou número ' + chute);
-};
+// Enquanto
+while (chute != numeroSecreto) {
 
-//Nova linha
+    chute = prompt("Digite seu número entre 1 e 10");
+
+    // Condicional
+    if (chute == numeroSecreto) {
+    
+        alert(`Você acertou!! O número secreto é o ${numeroSecreto} e teve ${tentativas} tentativas.`);
+        console.log(numeroSecreto);
+    } else {
+
+        if (chute < numeroSecreto) {
+            alert(`Você errou, o número é maior que ${chute}`);
+        } else {
+            alert(`Você errou, o número é menor que ${chute}`);
+        }
+
+        // contabiliza quantas vezes o usuário tentou acertar o número secreto
+        tentativas++
+    }
+}
