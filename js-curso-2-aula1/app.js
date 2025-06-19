27a922b6049b7d2419f5e4c317fcc9c6e24d99ee
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-06-19 20:59:38 -0300

    Aula de função

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'JOGO DO NÚMERO SECRETO';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10.';
+
+function verificarChute() {
+    console.log("O botão foi clicado");
+};
