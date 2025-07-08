6d31eb161859df3b1c9f9adcdedb2b3535913aa8
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-07-08 17:41:06 -0300

    Aula de função

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,9 +1,20 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'JOGO DO NÚMERO SECRETO';
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10.';
+exibirTextoNaTela('h1', 'JOGO DO NÚMERO SECRETO');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute() {
+    let chute = document.querySelector('input').value;
     console.log("O botão foi clicado");
+    console.log(chute == numeroSecreto);
 };
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1)
+}
