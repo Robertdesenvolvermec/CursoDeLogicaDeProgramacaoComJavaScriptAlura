52a0405f2a0ff5c9c5918d22c41ced1faa909587
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-07-11 18:27:21 -0300

    Segunda parte feita

--- logica-js-desafio-aula-8-funcoes/app.js
+++ logica-js-desafio-aula-8-funcoes/app.js
@@ -2,8 +2,14 @@ let nome;
 
 console.log('Olá, mundo!');
 menagemConsole('Robert');
+console.log(retornaNumeroDobrado(1));
+console.log(retornaNumeroDobrado(2));
 
 function menagemConsole(nome) {
     console.log(`Olá, ${nome}`);
+}
+
+function retornaNumeroDobrado(numeroDobrado) {
+    return numeroDobrado * 2;
 }
 
