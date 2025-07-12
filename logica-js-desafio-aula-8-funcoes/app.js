a3498578cc2885e8d8c802ba464bb0255eac45b6
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-07-12 16:21:44 -0300

    Desafio da Função Concluído

--- logica-js-desafio-aula-8-funcoes/app.js
+++ logica-js-desafio-aula-8-funcoes/app.js
@@ -3,7 +3,11 @@ let nome;
 console.log('Olá, mundo!');
 menagemConsole('Robert');
 console.log(retornaNumeroDobrado(1));
-console.log(retornaNumeroDobrado(2));
+console.log(retornaMediaNumero(3,6,12));
+console.log(`retorna o maior número entre 1 e 2: ${retornaMaiorNumero(1,2)}`);
+console.log(`retorna o maior número entre 3 e 2: ${retornaMaiorNumero(3,2)}`);
+console.log(`retorna o resultado da multiplicação do próprio número 3: ${retornaNumeroMultiplicadoPorEleMesmo(3)}`);
+
 
 function menagemConsole(nome) {
     console.log(`Olá, ${nome}`);
@@ -13,3 +17,27 @@ function retornaNumeroDobrado(numeroDobrado) {
     return numeroDobrado * 2;
 }
 
+function retornaMediaNumero(numero1, numero2, numero3) {
+    return media = (numero1 + numero2 + numero3) / 3;
+}
+
+function retornaMaiorNumero(numero1, numero2) {
+    if (numero1 > numero2) {
+        return numero1;        
+    } else {
+        return numero2;
+    }
+}
+
+// uma solução alternativa de operador ternário para condições:
+function encontrarMaior(a, b) {
+  return a > b ? a : b;
+}
+
+let maiorNumero = encontrarMaior(15, 9);
+console.log(maiorNumero);
+
+
+function retornaNumeroMultiplicadoPorEleMesmo(numeroMult) {
+    return numeroMult * numeroMult;    
+}
