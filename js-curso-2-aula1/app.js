3623788806e314367e2b67bf109b64a21cb6aa20
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2025-07-15 22:41:11 -0300

    Adicionei a lógica no jogo em verificarChute

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,20 +1,29 @@
 let numeroSecreto = gerarNumeroAleatorio();
 console.log(numeroSecreto);
 
-exibirTextoNaTela('h1', 'JOGO DO NÚMERO SECRETO');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela("h1", "JOGO DO NÚMERO SECRETO");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
 function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log("O botão foi clicado");
-    console.log(chute == numeroSecreto);
-};
+  let chute = document.querySelector("input").value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Parabéns!!");
+    exibirTextoNaTela("p", "Você acertou!!");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "Você errou, o número é menor.");
+    } else {
+      exibirTextoNaTela("p", "Você errou, o número é maior.");
+    }
+  }
+}
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1)
+  return parseInt(Math.random() * 10 + 1);
 }
